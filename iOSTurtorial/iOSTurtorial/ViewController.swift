d0e1134051ca9d21455b2429a962173fa7fb1212
Kyosuke Nakanishi <kyoro920@gmail.com>
2019-02-18 23:28:55 +0900

    ボタンをタップするとラベルに現在時刻が表示されるようにした

--- iOSTurtorial/iOSTurtorial/ViewController.swift
+++ iOSTurtorial/iOSTurtorial/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func currentTimeButtonTapped(_ sender: Any) {
+        let now = Date() // 現在時刻を取得
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current // ロケールを指定
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss.SSS" // 時刻表示のフォーマットを指定
+        // 指定したフォーマットで現在時刻を取得してラベルに反映させる
+        timeLabel.text = dateFormatter.string(from: now)
+    }
+    
 }
 

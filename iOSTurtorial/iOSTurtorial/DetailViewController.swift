a891b69065e1dcdd55b68b97ca6eee7cfe079887
Kyosuke Nakanishi <kyoro920@gmail.com>
2019-02-19 04:07:43 +0900

    MapViewを追加。地名から座標取得してMapViewの表示範囲に反映。

--- iOSTurtorial/iOSTurtorial/DetailViewController.swift
+++ iOSTurtorial/iOSTurtorial/DetailViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
     var titleStr:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         titleLabel.text = titleStr
+        
+        if let placeName = titleStr {
+            CLGeocoder().geocodeAddressString(placeName) { placemarks, error in
+                guard let coordinate = placemarks?.first?.location?.coordinate else {return}
+                
+                //中心座標
+                let center = coordinate
+                
+                //表示範囲
+                let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+                
+                //中心座標と表示範囲をマップに登録する。
+                let region = MKCoordinateRegion(center: center, span: span)
+                self.mapView.setRegion(region, animated:false)
+            }
+        }
+        
     }
 
 }
